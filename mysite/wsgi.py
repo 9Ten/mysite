453f352cfee86a2c26a20a0880f7cc2bd6f6c96d
FLY100% <panapot@hotmail.com>
2018-10-02 12:41:19 +0700

    update wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,15 +10,6 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-<<<<<<< HEAD
-=======
-# from whitenoise.django import DjangoWhiteNoise
->>>>>>> 10bef286ea21e1a42225756e8cac5a3192f294fd
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
 application = get_wsgi_application()
-<<<<<<< HEAD
-=======
-# application = DjangoWhiteNoise(application)
->>>>>>> 10bef286ea21e1a42225756e8cac5a3192f294fd
