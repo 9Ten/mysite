66e5235136444fbe90352b72d983fbf79b08cdb0
Phonkornkrit <phonkorp@ais.co.th>
2018-10-04 23:17:23 +0700

    Modification request from users

--- mysite/static/js/program.js
+++ mysite/static/js/program.js
@@ -1,22 +1,52 @@
 $( document ).ready(function() {
   var list_date  = [
-    "Sunday November 19th",
-    "Monday November 20th",
-    "Tuesday November 21st",
-    "Wednesday November 22nd"
+    "Sunday November 18th",
+    "Monday November 19th",
+    "Tuesday November 20th",
+    "Wednesday November 21st",
+    "Thursday November 22nd"
    ]
   var list_event =  [
     [
-      "xx:xx-xx:xx","Waiting For Data",
+      "12:00–18:00","Registration",
     ],
     [
-      "xx:xx-xx:xx","Waiting For Data",
+      "8:00–8:30","Registration (cont.)",
+      "8:30–9:00","Welcoming address",
+      "9:00–10:00","Plenary 1 – Robert J. Raven",
+      "10:00–10:30","Coffee break",
+      "10:30–12:30","Oral session",
+      "12:30–14:00","Lunch",
+      "14:00–16:00","Oral session",
+      "16:00–16:30","Coffee break",
+      "16:30–17:30","Poster session",
+      "18:30","Welcoming banquet in meeting hall floor 20"
     ],
     [
-      "xx:xx-xx:xx","Waiting For Data",
+      "8:00–9:00","Registration (cont.)",
+      "9:00–10:00","Plenary 2 – Deborah R. R. Smith",
+      "10:00–10:30","Coffee break",
+      "10:30–12:30","Oral session",
+      "12:30–14:00","Lunch",
+      "14:00–16:00","Oral session",
+      "16:00–16:30","Coffee break",
+      "16:30–17:30","Oral session",
+      "17:30–18:30","Poster session"
     ],
     [
-      "xx:xx-xx:xx","Waiting For Data",
+      "7:00–9:00","Bus to KMUTT, Ratchaburi campus",
+      "9:00–17:00","Excursion",
+      "17:00–19:00","Bus to CU"
+    ],
+    [
+      "9:00–10:00","Oral session",
+      "10:00–10:30","Coffee break",
+      "10:30–12:30","Oral session",
+      "12:30–14:00","Lunch",
+      "14:00–16:00","Oral session",
+      "16:00–16:30","Coffee break",
+      "16:30–18:30","Poster award and closing session",
+
     ]
   ]
   var table = document.createElement('table');
