e8323cd8cbb848e3780f3e49a98ef829ecdcf7af
panapot poungnak <panapot@hotmail.com>
2018-10-11 14:25:14 +0700

    update deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -151,7 +151,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
